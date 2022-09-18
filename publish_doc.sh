cb6ce7327875205d11b8c96477235617e87dffe8
Ron Zuckerman <ron.zuckerman@adtran.com>
2022-09-18 18:51:46 -0500

    Try to use token

--- publish_doc.sh
+++ publish_doc.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 REMOTE="$(git remote -v | awk '{ print $2 }' | head -1)"
+if [ -n "${GITHUB_TOKEN}" ]
+then
+    REMOTE=$(echo "${REMOTE}" | sed 's!!https://!https://'"rzuckerm:${GITHUB_TOKEN}"'@!')
+fi
+
 set -x
 cd "$1"
 git init .
 git config user.email "automation@example.com"
 git config user.name "Automation"
+set +x
 git remote add origin "${REMOTE}"
+set -x
 git add .
 git commit -m "Generated documentation"
 git push -f origin master:gh-pages
