8130121c842a318c187d50ac275a72a8380ead0e
Ron Zuckerman <ron.zuckerman@adtran.com>
2022-09-18 18:22:07 -0500

    Set username/email after 'git init'

--- publish_doc.sh
+++ publish_doc.sh
@@ -2,9 +2,9 @@
 REMOTE="$(git remote -v | awk '{ print $2 }' | head -1)"
 set -x
 cd "$1"
+git init .
 git config user.email "automation@example.com"
 git config user.name "Automation"
-git init .
 git remote add origin "${REMOTE}"
 git add .
 git commit -m "Generated documentation"
