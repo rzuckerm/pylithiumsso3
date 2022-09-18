7d65e923a2e3d3873011ea5fc1947433062fbdc9
Ron Zuckerman <ron.zuckerman@adtran.com>
2022-09-18 18:19:02 -0500

    Need to set username/email in publish docs

--- publish_doc.sh
+++ publish_doc.sh
@@ -2,8 +2,10 @@
 REMOTE="$(git remote -v | awk '{ print $2 }' | head -1)"
 set -x
 cd "$1"
+git config user.email "automation@example.com"
+git config user.name "Automation"
 git init .
 git remote add origin "${REMOTE}"
 git add .
 git commit -m "Generated documentation"
-git push -f origin main:gh-pages
+git push -f origin master:gh-pages
